114a8aa494d735f33504927bf26cb2b6912cf2cf
Ignacio Pablo <ignaciopabloro2@gmail.com>
2018-04-04 14:59:07 -0500

    Inicialización.

--- app/src/main/java/ipn/proyectoipod/com/xiimba/MainActivity.java
+++ app/src/main/java/ipn/proyectoipod/com/xiimba/MainActivity.java
@@ -2,13 +2,21 @@ package ipn.proyectoipod.com.xiimba;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText txtUsuario, txtPass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // Aqui un cambio
+        iniciarComponentes();
+    }
+
+    private void iniciarComponentes(){
+        txtUsuario = (EditText)findViewById(R.id.Inicio_txtUsuario);
+        txtPass = (EditText)findViewById(R.id.Inicio_txtPass);
     }
 }
